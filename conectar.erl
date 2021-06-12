3c31295c5ad893405a3e5d182264ee8f1f3f9a4c
Francisco Bolzan <franblz99@gmail.com>
2021-06-12 10:49:51 -0300

    Fixed minor error

--- conectar.erl
+++ conectar.erl
@@ -4,5 +4,5 @@
 con() ->
     net_kernel:connect_node(node1@FranPC),
     %rpc:call(node1@FranPC, node, start, []),
-    rpc:call(node2@FranPC, node, start, []),
-    rpc:call(node3@FranPC, node, start, []).
+    net_kernel:connect_node(node2@FranPC),
+    net_kernel:connect_node(node3@FranPC).
